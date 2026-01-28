ff22fd499f811826f69fc3a50e0f726f12926bef
Khadije-git <23014@supnum.mr>
2026-01-28 00:38:01 +0000

    refactor: Suppression des fichiers REST de la branche SOAP

--- src/main/java/com/supnum/td1_servers/dto/RenameServerRequest.java
+++
@@ -1,9 +0,0 @@
-package com.supnum.td1_servers.dto;
-
-public class RenameServerRequest {
-    private String newName;
-
-    public String getNewName() { return newName; }
-
-    public void setNewName(String newName) { this.newName = newName; }
-}
